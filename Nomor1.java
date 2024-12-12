3a44ba23ed74c9c83f5e450edd87390b13779415
dikialiftaufik <dikialift13@gmail.com>
2024-12-12 09:46:11 +0700

    update Nomor1

--- Nomor1.java
+++ Nomor1.java
@@ -7,14 +7,14 @@ public class Nomor1 {
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
         for (int i = 1; i <= N; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+            if (i % 15 == 0) {
                 System.out.print("FizzBuzz ");
             } else if (i % 3 == 0) {
                 System.out.print("Fizz ");
             } else if (i % 5 == 0) {
                 System.out.print("Buzz ");
             } else {
-                System.out.print(i + " ");
+                System.out.print(i);
             }
         }
         /* Pekerjaan anda berakhir sini */
