de01a9a73e99a5ffae99bca2f152e00c985ea047
dikialiftaufik <dikialift13@gmail.com>
2024-12-12 10:00:17 +0700

    update Nomor1

--- Nomor1.java
+++ Nomor1.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
 
 public class Nomor1 {
+    // Prosedur untuk menampilkan deret FizzBuzz
+    public static void tampilkanFizzBuzz(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
-        for (int i = 1; i <= N; i++) {
-            if (i % 15 == 0) {
-                System.out.print("FizzBuzz ");
-            } else if (i % 3 == 0) {
-                System.out.print("Fizz ");
-            } else if (i % 5 == 0) {
-                System.out.print("Buzz ");
-            } else {
-                System.out.print(i);
-            }
-        }
-        /* Pekerjaan anda berakhir sini */
-
+        tampilkanFizzBuzz(N);
+        /* Pekerjaan anda berakhir sisi */
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
     }
-    
 }
